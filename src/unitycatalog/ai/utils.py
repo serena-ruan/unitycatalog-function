7087fff77a8c75d567465dd4f7f958a5d2964a06
serena-ruan <serena.rxy@gmail.com>
2024-09-12 14:58:45 +0800

    support convert arg info to pydantic model
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- src/unitycatalog/ai/utils.py
+++ src/unitycatalog/ai/utils.py
@@ -1,44 +1,59 @@
-from typing import Any, NamedTuple
 import datetime
+import base64
 import decimal
+import json
+from dataclasses import dataclass
+from hashlib import md5
+from typing import Any, Dict, List, NamedTuple, Optional, Tuple, Type, Union
+
+# TODO: validate this against pydantic v1 and v2
+from pydantic import Field, create_model, BaseModel
+
+SQL_TYPE_TO_PYTHON_TYPE_MAPPING = {
+    # numpy array is not accepted, it's not json serializable
+    "ARRAY": (list, tuple),
+    "BINARY": (bytes, str),
+    "BOOLEAN": bool,
+    # tinyint type
+    "BYTE": int,
+    "CHAR": str,
+    "DATE": (datetime.date, str),
+    # no precision and scale check, rely on SQL function to validate
+    "DECIMAL": (decimal.Decimal, float),
+    "DOUBLE": float,
+    "FLOAT": float,
+    "INT": int,
+    "INTERVAL": (datetime.timedelta, str),
+    "LONG": int,
+    "MAP": dict,
+    # ref: https://docs.databricks.com/en/error-messages/datatype-mismatch-error-class.html#null_type
+    # it's not supported in return data type as well `[UNSUPPORTED_DATATYPE] Unsupported data type "NULL". SQLSTATE: 0A000`
+    "NULL": type(None),
+    "SHORT": int,
+    "STRING": str,
+    "STRUCT": dict,
+    # not allowed for python udf, users should only pass string
+    "TABLE_TYPE": str,
+    "TIMESTAMP": (datetime.datetime, str),
+    "TIMESTAMP_NTZ": (datetime.datetime, str),
+    # it's a type that can be defined in scala, python shouldn't force check the type here
+    # ref: https://www.waitingforcode.com/apache-spark-sql/used-defined-type/read
+    "USER_DEFINED_TYPE": object,
+}
+
+UC_TYPE_JSON_MAPPING = {
+    **SQL_TYPE_TO_PYTHON_TYPE_MAPPING,
+    "INTEGER": int,
+    # The binary field should be a string expression in base64 format
+    "BINARY": bytes,
+    "INTERVAL DAY TO SECOND": (datetime.timedelta, str),
+}
 
 
 def column_type_to_python_type(column_type: str) -> Any:
-    mapping = {
-        # numpy array is not accepted, it's not json serializable
-        "ARRAY": (list, tuple),
-        # a string expression in base64 format
-        "BINARY": str,
-        "BOOLEAN": bool,
-        # tinyint type
-        "BYTE": int,
-        "CHAR": str,
-        "DATE": (datetime.date, str),
-        # no precision and scale check, rely on SQL function to validate
-        "DECIMAL": (decimal.Decimal, float),
-        "DOUBLE": float,
-        "FLOAT": float,
-        "INT": int,
-        "INTERVAL": (datetime.timedelta, str),
-        "LONG": int,
-        "MAP": dict,
-        # ref: https://docs.databricks.com/en/error-messages/datatype-mismatch-error-class.html#null_type
-        # it's not supported in return data type as well `[UNSUPPORTED_DATATYPE] Unsupported data type "NULL". SQLSTATE: 0A000`
-        "NULL": type(None),
-        "SHORT": int,
-        "STRING": str,
-        "STRUCT": dict,
-        # not allowed for python udf, users should only pass string
-        "TABLE_TYPE": str,
-        "TIMESTAMP": (datetime.datetime, str),
-        "TIMESTAMP_NTZ": (datetime.datetime, str),
-        # it's a type that can be defined in scala, python shouldn't force check the type here
-        # ref: https://www.waitingforcode.com/apache-spark-sql/used-defined-type/read
-        "USER_DEFINED_TYPE": object,
-    }
-    if column_type not in mapping:
+    if column_type not in SQL_TYPE_TO_PYTHON_TYPE_MAPPING:
         raise ValueError(f"Unsupported column type: {column_type}")
-    return mapping[column_type]
+    return SQL_TYPE_TO_PYTHON_TYPE_MAPPING[column_type]
 
 
 def is_time_type(column_type: str) -> bool:
@@ -78,6 +93,19 @@ def validate_param(param: Any, column_type: str, param_type_text: str) -> None:
             raise ValueError(
                 f"Invalid interval string: {param}, expecting format `INTERVAL '[+|-] d[...] [h]h:[m]m:[s]s.ms[ms][ms][us][us][us]' DAY TO SECOND`."
             )
+    elif column_type == "BINARY" and isinstance(param, str) and not is_base64_encoded(param):
+        # the string value for BINARY column must be base64 encoded
+        raise ValueError(
+            f"The string input for column type BINARY must be base64 encoded, invalid input: {param}."
+        )
+
+
+def is_base64_encoded(s: str) -> bool:
+    try:
+        base64.b64decode(s, validate=True)
+        return True
+    except (base64.binascii.Error, ValueError):
+        return False
 
 
 def convert_timedelta_to_interval_str(time_val: datetime.timedelta) -> str:
@@ -113,3 +141,148 @@ def validate_full_function_name(function_name: str) -> FullFunctionName:
             f"Invalid function name: {function_name}, expecting format <catalog_name>.<schema_name>.<function_name>."
         )
     return FullFunctionName(catalog_name=splits[0], schema_name=splits[1], function_name=splits[2])
+
+
+def uc_type_json_to_pydantic_type(uc_type_json: Union[str, Dict]) -> Type:
+    """
+    Convert Unity Catalog type json to Pydantic type.
+    """
+    if isinstance(uc_type_json, str):
+        type_name = uc_type_json.upper()
+        if type_name in UC_TYPE_JSON_MAPPING:
+            return Union[UC_TYPE_JSON_MAPPING[type_name]]
+        # the type text contains the precision and scale
+        elif type_name.startswith("DECIMAL"):
+            return Union[decimal.Decimal, float]
+        else:
+            raise TypeError(
+                f"Type {uc_type_json} is not supported. Supported "
+                f"types are: {UC_TYPE_JSON_MAPPING.keys()}"
+            )
+    if isinstance(uc_type_json, dict):
+        tpe = uc_type_json["type"]
+        if tpe == "array":
+            element_type = uc_type_json_to_pydantic_type(uc_type_json["elementType"])
+            if uc_type_json["containsNull"]:
+                element_type = Optional[element_type]
+            return Union[List[element_type], Tuple[element_type]]
+        elif tpe == "map":
+            key_type = uc_type_json["keyType"]
+            if key_type != "string":
+                raise TypeError(f"Only support STRING key type for MAP but got {key_type}.")
+            value_type = uc_type_json_to_pydantic_type(uc_type_json["valueType"])
+            if uc_type_json["valueContainsNull"]:
+                value_type = Optional[value_type]
+            return Dict[str, value_type]
+        elif tpe == "struct":
+            fields = {}
+            for field in uc_type_json["fields"]:
+                field_type = uc_type_json_to_pydantic_type(field["type"])
+                comment = field.get("metadata", {}).get("comment")
+                if field.get("nullable"):
+                    field_type = Optional[field_type]
+                    fields[field["name"]] = (field_type, Field(default=None, description=comment))
+                else:
+                    fields[field["name"]] = (field_type, Field(..., description=comment))
+            uc_type_json_str = json.dumps(uc_type_json, sort_keys=True)
+            type_hash = md5(uc_type_json_str.encode()).hexdigest()[:8]
+            return create_model(f"Struct_{type_hash}", **fields)
+    raise TypeError(f"Unknown type {uc_type_json}.")
+
+
+# TODO: add UC OSS support
+def supported_param_info_types():
+    types = ()
+    try:
+        from databricks.sdk.service.catalog import FunctionParameterInfo
+
+        types += (FunctionParameterInfo,)
+    except ImportError:
+        pass
+
+    return types
+
+
+# TODO: add UC OSS support
+def supported_function_info_types():
+    types = ()
+    try:
+        from databricks.sdk.service.catalog import FunctionInfo
+
+        types += (FunctionInfo,)
+    except ImportError:
+        pass
+
+    return types
+
+
+@dataclass
+class PydanticField:
+    pydantic_type: Type
+    description: Optional[str] = None
+    default: Optional[Any] = None
+
+
+def param_info_to_pydantic_type(param_info: Any) -> PydanticField:
+    """
+    Convert Unity Catalog function parameter information to Pydantic type.
+
+    Args:
+        param_info: The Unity Catalog function parameter information.
+            It must be either databricks.sdk.service.catalog.FunctionParameterInfo or
+            unitycatalog.types.function_info.InputParamsParameter object.
+    """
+    if not isinstance(param_info, supported_param_info_types()):
+        raise TypeError(f"Unsupported parameter info type: {type(param_info)}")
+    if param_info.type_json is None:
+        raise ValueError(f"Parameter type json is None for parameter {param_info.name}.")
+    type_json = json.loads(param_info.type_json)
+    nullable = type_json.get("nullable")
+    pydantic_type = uc_type_json_to_pydantic_type(type_json["type"])
+    if nullable:
+        pydantic_type = Optional[pydantic_type]
+    default = None
+    description = param_info.comment or ""
+    if param_info.parameter_default:
+        pydantic_type = Optional[pydantic_type]
+        # Note: DEFAULT is supported for LANGUAGE SQL only.
+        # TODO: verify this for all types
+        default = json.loads(param_info.parameter_default)
+        description = f"{description} (Default: {param_info.parameter_default})"
+    return PydanticField(
+        pydantic_type=pydantic_type,
+        description=description,
+        default=default,
+    )
+
+
+def generate_function_input_params_schema(function_info: Any) -> Type[BaseModel]:
+    """
+    Generate a Pydantic model based on a Unity Catalog function information.
+
+    Args:
+        function_info: The Unity Catalog function information.
+            It must either be databricks.sdk.service.catalog.FunctionInfo or
+            unitycatalog.types.function_info.FunctionInfo object.
+
+    Returns:
+        The Pydantic model representing the input parameters of the function.
+    """
+    if not isinstance(function_info, supported_function_info_types()):
+        raise TypeError(f"Unsupported function info type: {type(function_info)}")
+    if function_info.input_params is None:
+        return BaseModel
+    param_infos = function_info.input_params.parameters
+    if param_infos is None:
+        raise ValueError("Function input parameters are None.")
+    fields = {}
+    for param_info in param_infos:
+        pydantic_field = param_info_to_pydantic_type(param_info)
+        fields[param_info.name] = (
+            pydantic_field.pydantic_type,
+            Field(default=pydantic_field.default, description=pydantic_field.description),
+        )
+    return create_model(
+        f"{function_info.catalog_name}__{function_info.schema_name}__{function_info.name}__params",
+        **fields,
+    )
--- tests/ai/test_utils.py
+++ tests/ai/test_utils.py
@@ -1,9 +1,20 @@
 import pytest
 import datetime
+import decimal
+from typing import Union, List, Optional, Tuple, Dict
+from pydantic import BaseModel
 from unitycatalog.ai.utils import (
     column_type_to_python_type,
     convert_timedelta_to_interval_str,
     validate_full_function_name,
+    uc_type_json_to_pydantic_type,
+    generate_function_input_params_schema,
+    is_base64_encoded,
+)
+from databricks.sdk.service.catalog import (
+    FunctionParameterInfo,
+    FunctionInfo,
+    FunctionParameterInfos,
 )
 
 
@@ -22,6 +33,11 @@ def test_column_type_to_python_type_errors():
         column_type_to_python_type("INVALID_TYPE")
 
 
+def test_is_base64_encoded():
+    assert is_base64_encoded("aGVsbG8=")
+    assert not is_base64_encoded("hello")
+
+
 @pytest.mark.parametrize(
     ("time_val", "expected"),
     [
@@ -35,3 +51,351 @@ def test_column_type_to_python_type_errors():
 )
 def test_convert_timedelta_to_interval_str(time_val, expected):
     assert convert_timedelta_to_interval_str(time_val) == expected
+
+
+@pytest.mark.parametrize(
+    ("uc_type_json", "expected_type_or_input"),
+    [
+        (
+            {"type": "array", "elementType": "byte", "containsNull": True},
+            Union[List[Optional[int]], Tuple[Optional[int]]],
+        ),
+        (
+            {
+                "type": "struct",
+                "fields": [
+                    {
+                        "name": "a",
+                        "type": "short",
+                        "nullable": False,
+                        "metadata": {"comment": "short field"},
+                    },
+                    {
+                        "name": "b",
+                        "type": {
+                            "type": "map",
+                            "keyType": "string",
+                            "valueType": "float",
+                            "valueContainsNull": True,
+                        },
+                        "nullable": True,
+                        "metadata": {},
+                    },
+                    {"name": "c", "type": "integer", "nullable": False, "metadata": {}},
+                ],
+            },
+            {"a": 123, "b": {"key": 123.0}, "c": 123},
+        ),
+        ("binary", bytes),
+        ("interval day to second", Union[datetime.timedelta, str]),
+        ("timestamp", Union[str, datetime.datetime]),
+        ("timestamp_ntz", Union[str, datetime.datetime]),
+        ("date", Union[str, datetime.date]),
+        (
+            {
+                "type": "map",
+                "keyType": "string",
+                "valueType": {"type": "array", "elementType": "integer", "containsNull": True},
+                "valueContainsNull": True,
+            },
+            Dict[str, Optional[Union[List[Optional[int]], Tuple[Optional[int]]]]],
+        ),
+        ("decimal(10,2)", Union[float, decimal.Decimal]),
+        ("double", float),
+        ("float", float),
+        ("long", int),
+        ("boolean", bool),
+        ("integer", int),
+        ("short", int),
+        ("string", str),
+        (
+            {
+                "type": "array",
+                "elementType": {"type": "array", "elementType": "string", "containsNull": False},
+                "containsNull": False,
+            },
+            Union[List[Union[List[str], Tuple[str]]], Tuple[Union[List[str], Tuple[str]]]],
+        ),
+    ],
+)
+def test_uc_type_json_to_pydantic_type(uc_type_json, expected_type_or_input):
+    param_type = uc_type_json_to_pydantic_type(uc_type_json)
+    if isinstance(param_type, type(BaseModel)):
+        param_type(**expected_type_or_input)
+    else:
+        assert param_type == expected_type_or_input
+
+
+def generate_function_info(parameters: List[Dict]):
+    return FunctionInfo(
+        catalog_name="catalog",
+        schema_name="schema",
+        name="test",
+        input_params=FunctionParameterInfos(
+            parameters=[FunctionParameterInfo(**param) for param in parameters]
+        ),
+    )
+
+
+@pytest.mark.parametrize(
+    ("function_info", "valid_inputs"),
+    [
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "a",
+                        "type_text": "struct<a:smallint,b:map<string,float>,c:int>",
+                        "type_json": '{"name":"a","type":{"type":"struct","fields":[{"name":"a","type":"short","nullable":false,"metadata":{"comment":"short field"}},{"name":"b","type":{"type":"map","keyType":"string","valueType":"float","valueContainsNull":true},"nullable":true,"metadata":{}},{"name":"c","type":"integer","nullable":false,"metadata":{}}]},"nullable":true,"metadata":{}}',
+                        "type_name": "STRUCT",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 0,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [
+                {"a": {"a": 123, "b": {"key": 123.0}, "c": 123}},
+                {"a": {"a": 123, "b": {"key": None}, "c": 123}},
+                {"a": {"a": 123, "b": None, "c": 234}},
+                {"a": {"a": 0, "c": 234}},
+            ],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "b",
+                        "type_text": "array<tinyint>",
+                        "type_json": '{"name":"b","type":{"type":"array","elementType":"byte","containsNull":true},"nullable":true,"metadata":{}}',
+                        "type_name": "ARRAY",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 1,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"b": [100] * 10}, {}, {"b": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "c",
+                        "type_text": "binary",
+                        "type_json": '{"name":"c","type":"binary","nullable":true,"metadata":{"comment":"BINARY field"}}',
+                        "type_name": "BINARY",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 2,
+                        "parameter_type": "PARAM",
+                        "comment": "BINARY field",
+                    }
+                ]
+            ),
+            [{"c": b"binary"}, {"c": "aGVsbG8="}, {}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "d",
+                        "type_text": "interval day to second",
+                        "type_json": '{"name":"d","type":"interval day to second","nullable":true,"metadata":{}}',
+                        "type_name": "INTERVAL",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 3,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"d": datetime.timedelta(days=1, seconds=1, microseconds=123456)}, {"d": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "e",
+                        "type_text": "timestamp",
+                        "type_json": '{"name":"e","type":"timestamp","nullable":true,"metadata":{}}',
+                        "type_name": "TIMESTAMP",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 4,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"e": datetime.datetime(2024, 9, 11)}, {"e": "2024-09-11T00:00:00"}, {"e": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "f",
+                        "type_text": "timestamp_ntz",
+                        "type_json": '{"name":"f","type":"timestamp_ntz","nullable":true,"metadata":{}}',
+                        "type_name": "TIMESTAMP_NTZ",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 5,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"f": datetime.datetime(2024, 9, 11)}, {"f": "2024-09-11T00:00:00"}, {"f": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "g",
+                        "type_text": "date",
+                        "type_json": '{"name":"g","type":"date","nullable":true,"metadata":{}}',
+                        "type_name": "DATE",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 6,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"g": datetime.date(2024, 9, 11)}, {"g": "2024-09-11"}, {"g": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "h",
+                        "type_text": "map<string,array<int>>",
+                        "type_json": '{"name":"h","type":{"type":"map","keyType":"string","valueType":{"type":"array","elementType":"integer","containsNull":true},"valueContainsNull":true},"nullable":true,"metadata":{}}',
+                        "type_name": "MAP",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 7,
+                        "parameter_type": "PARAM",
+                    }
+                ]
+            ),
+            [{"h": {"key": [1, 2, 3], "key2": None}}, {"h": {"key": None}}, {"h": None}],
+        ),
+        (
+            generate_function_info(
+                [
+                    {
+                        "name": "i",
+                        "type_text": "decimal(10,2)",
+                        "type_json": '{"name":"i","type":"decimal(10,2)","nullable":true,"metadata":{}}',
+                        "type_name": "DECIMAL",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 8,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "j",
+                        "type_text": "double",
+                        "type_json": '{"name":"j","type":"double","nullable":true,"metadata":{}}',
+                        "type_name": "DOUBLE",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 9,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "k",
+                        "type_text": "float",
+                        "type_json": '{"name":"k","type":"float","nullable":true,"metadata":{}}',
+                        "type_name": "FLOAT",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 10,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "l",
+                        "type_text": "bigint",
+                        "type_json": '{"name":"l","type":"long","nullable":true,"metadata":{}}',
+                        "type_name": "LONG",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 11,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "m",
+                        "type_text": "boolean",
+                        "type_json": '{"name":"m","type":"boolean","nullable":true,"metadata":{}}',
+                        "type_name": "BOOLEAN",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 12,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "o",
+                        "type_text": "int",
+                        "type_json": '{"name":"o","type":"integer","nullable":true,"metadata":{}}',
+                        "type_name": "INT",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 14,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "p",
+                        "type_text": "smallint",
+                        "type_json": '{"name":"p","type":"short","nullable":true,"metadata":{}}',
+                        "type_name": "SHORT",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 15,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "q",
+                        "type_text": "array<array<string>>",
+                        "type_json": '{"name":"q","type":{"type":"array","elementType":{"type":"array","elementType":"string","containsNull":true},"containsNull":true},"nullable":true,"metadata":{}}',
+                        "type_name": "ARRAY",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 16,
+                        "parameter_type": "PARAM",
+                    },
+                    {
+                        "name": "r",
+                        "type_text": "string",
+                        "type_json": '{"name":"r","type":"string","nullable":true,"metadata":{"EXISTS_DEFAULT":"\\"123\\"","default":"\\"123\\"","CURRENT_DEFAULT":"\\"123\\""}}',
+                        "type_name": "STRING",
+                        "type_precision": 0,
+                        "type_scale": 0,
+                        "position": 17,
+                        "parameter_type": "PARAM",
+                        "parameter_default": '"123"',
+                    },
+                ]
+            ),
+            [
+                {
+                    "i": 1.234,
+                    "j": 1.234,
+                    "k": 1.234,
+                    "l": 123,
+                    "m": True,
+                    "o": 123,
+                    "p": 123,
+                    "q": [["a", "b"], ["c"], ["d", None], None],
+                    "r": "123",
+                },
+                {},
+            ],
+        ),
+    ],
+)
+def test_generate_function_input_params_schema(function_info, valid_inputs):
+    pydantic_model = generate_function_input_params_schema(function_info)
+    for data in valid_inputs:
+        pydantic_model(**data)
