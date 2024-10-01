7f8dd694939cea76694b0a5ee69a7bcbc0383886
serena-ruan_data <serena.ruan@databricks.com>
2024-10-01 14:01:25 +0800

    update
    Signed-off-by: serena-ruan_data <serena.ruan@databricks.com>

--- dev/databricks_functions_cleanup.py
+++ dev/databricks_functions_cleanup.py
@@ -3,19 +3,22 @@ import sys
 import click
 from databricks.sdk import WorkspaceClient
 
-from ucai.test_utils.function_utils import CATALOG
-
 
 @click.command(help="Clean up functions in a schema")
 @click.option(
+    "--catalog",
+    required=True,
+    help=f"The catalog to clean up functions from",
+)
+@click.option(
     "--schema",
     required=True,
-    help=f"The schema to clean up functions from, under catalog {CATALOG}",
+    help=f"The schema to clean up functions from",
 )
-def cleanup_function(schema: str):
+def cleanup_function(catalog: str, schema: str):
     client = WorkspaceClient()
     failed_deletions = []
-    function_infos = client.functions.list(catalog_name=CATALOG, schema_name=schema)
+    function_infos = client.functions.list(catalog_name=catalog, schema_name=schema)
     for function_info in function_infos:
         try:
             client.functions.delete(function_info.full_name)
